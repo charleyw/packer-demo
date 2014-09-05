78d158e9b25ccbf23e041d4b38f0a97b3c3e3d2b
chao wang <cwang8023@gmail.com>
2014-09-05 13:47:56 +0800

    fix error

--- packer-scripts/deploy.sh
+++ packer-scripts/deploy.sh
@@ -1,8 +1,8 @@
 cd ~
 sudo apt-get install -y ruby1.9.3 git
-gem install bundler
+sudo gem install bundler
 
 git clone https://github.com/charleyw/packer-demo.git
-bundle install
+sudo bundle install
 
 sudo cp -f /tmp/rc.local /etc/rc.local
