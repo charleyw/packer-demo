4c1ae1b142b566b6a07ecb7e2616637c5a72394f
chao wang <cwang8023@gmail.com>
2014-09-05 13:41:42 +0800

    using file

--- packer-scripts/deploy.sh
+++ packer-scripts/deploy.sh
@@ -1,4 +1,3 @@
-sudo -s
 cd ~
 sudo apt-get install -y ruby1.9.3 git
 gem install bundler
@@ -6,6 +5,4 @@ gem install bundler
 git clone https://github.com/charleyw/packer-demo.git
 bundle install
 
-sudo sed -i '$ d' /etc/rc.local
-sudo echo "cd /home/ubuntu/packer-demo && bundle exec ruby -o 0.0.0.0 app.rb &" >> /etc/rc.local
-sudo echo "exit 0" >> /etc/rc.local
+sudo cp -f /tmp/rc.local /etc/rc.local
